d6de493879912457f3c56d8dc9d1ea231f3cd70a
James Skelton <d.j.skelton1@gmail.com>
2024-12-03 08:44:16 +0000

    Added day 3 solution, corrected day 2 benchmarks

--- benches/solution_benchmarks.rs
+++ benches/solution_benchmarks.rs
@@ -1,21 +1,32 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::hint::black_box;
 
-use aoc_2024::{day1, day2};
+use aoc_2024::{day1, day2, day3};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let contents = include_str!("../inputs/day01.txt");
+    let contents: &str = include_str!("../inputs/day01.txt");
     c.bench_function("Day 01, Part 1", |b| {
         b.iter(|| day1::part1(black_box(contents)))
     });
     c.bench_function("Day 01, Part 2", |b| {
         b.iter(|| day1::part2(black_box(contents)))
     });
+
+    let contents: &str = include_str!("../inputs/day02.txt");
     c.bench_function("Day 02, Part 1", |b| {
         b.iter(|| day2::part1(black_box(contents)))
     });
     c.bench_function("Day 02, Part 2", |b| {
         b.iter(|| day2::part2(black_box(contents)))
+    });
+
+    let contents: &str = include_str!("../inputs/day03.txt");
+
+    c.bench_function("Day 03, Part 1", |b| {
+        b.iter(|| day3::part1(black_box(contents)))
+    });
+    c.bench_function("Day 03, Part 2", |b| {
+        b.iter(|| day3::part2(black_box(contents)))
     });
 }
 
---
+++ src/day3.rs
@@ -0,0 +1,77 @@
+use regex::Regex;
+
+fn parse_radix_10(input: &str) -> u64 {
+    let mut n = 0;
+    for i in input.as_bytes() {
+        n *= 10;
+        n += *i as u64
+    }
+
+    let correction = {
+        match input.len() {
+            1 => 48,
+            2 => 528,
+            3 => 5_328,
+            4 => 53_328,
+            5 => 533_328,
+            6 => 5_333_328,
+            _ => unreachable!()
+
+        }
+    };
+
+    n -= correction;
+
+    return n
+}
+
+pub fn part1(content: &str) -> u64 {
+    let pattern = Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)").expect("could not parse regex pattern");
+    pattern.captures_iter(content).map(|i| {
+        let a = parse_radix_10(&i["a"]);
+        let b = parse_radix_10(&i["b"]);
+        a * b
+    }).sum()
+}
+
+
+pub fn part2(content: &str) -> u64 {
+    let pattern = Regex::new(r"(mul\((?<a>\d+),(?<b>\d+)\)|(?<dis>don\'t\(\))|(?<en>do\(\)))").expect("could not parse regex pattern");
+    let mut matching = true;
+
+    pattern.captures_iter(content).map(|i| {
+        if let Some(_) = i.name("dis") {
+            matching = false;
+            return 0
+        } else if let Some(_) = i.name("en") {
+            matching = true;
+            return 0
+        } else if matching {
+            let a = parse_radix_10(&i["a"]);
+            let b = parse_radix_10(&i["b"]);
+            return a * b
+        } else {
+            return 0
+        }
+    }).sum()
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one_solution() {
+        let contents = include_str!("../inputs/day03.txt");
+        let result = part1(&contents);
+        assert_eq!(result, 182_780_583);
+    }
+
+    #[test]
+    fn test_part_two_solution() {
+        let contents = include_str!("../inputs/day03.txt");
+        let result = part2(contents);
+        assert_eq!(result, 90_772_405);
+    }
+}
